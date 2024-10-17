75771711cc24195841710d2155b248c13548e166
miniicedlatte <areebaf818@gmail.com>
2024-10-17 18:21:15 +0500

    Create MJ2022Q2.py

---
+++ MJ2022Q2.py
@@ -0,0 +1,31 @@
+'''
+ArrayLength <- 10
+FOR X <- 0 TO ArrayLength - 1
+    FOR Y <- 0 TO ArrayLength - 2
+        FOR Z <- 0 TO ArrayLength - Y - 2
+            IF ArrayData[X, Z] > ArrayData[X, Z + 1] THEN
+                TempValue <- ArrayData[X, Z]
+                ArrayData[X, Z] <- ArrayData[X, Z+1]
+                ArrayData[X, Z + 1] <- TempValue
+            ENDIF
+        NEXT Z
+    NEXT Y
+NEXT X
+'''
+
+def printArr(array): #part c (print array)
+    for g in array:
+        print (g)
+
+printArr(arr) #part c (call before)
+print() #part c (seperator)
+
+# part b
+arrLen = 10
+for x in range(arrLen):
+    for y in range(arrLen-1):
+        for z in range(arrLen - y -1):
+            if arr[x][z] > arr[x][z+1] :
+                arr[x][z], arr[x][z+1] = arr[x][z+1], arr[x][z]
+
+printArr(arr) #part c (call after)
