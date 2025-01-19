c1766867aeec22cdfd4c4ab51856d2d0d0cfd916
miniicedlatte <areebaf818@gmail.com>
2025-01-19 22:08:46 +0500

    Create BT_OOP.py

---
+++ BT_OOP.py
@@ -0,0 +1,20 @@
+#node class definition
+class Node():
+#PRIVATE LeftPointer
+#PRIVATE RightPointer
+#PRIVATE Data
+def __init__（self,d）：
+seLf.-_LeftPointer = -1
+self.__Data = d
+self.-_RightPointer = -1
+def GetLefit(self) :
+return self.-_LeftPointer
+def Getnight(self):
+return self.-_RightPointer
+def GetData(self) :
+return self.-_Data
+def
+SetLeftPointer (self, Lp):
+self.-_LeftPointer = 2p
+def SetRightPointer(self, rp):
+self.-_RightPointer = rp
