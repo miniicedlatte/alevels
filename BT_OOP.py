3388ddef7a897a9e38c706ef44ab27c449f11247
miniicedlatte <areebaf818@gmail.com>
2025-01-19 22:14:58 +0500

    Update BT_OOP.py

--- BT_OOP.py
+++ BT_OOP.py
@@ -17,3 +17,34 @@ def SetLeftPointer (self, Lp):
   self.__LeftPointer = Lp
 def SetRightPointer(self, rp):
   self.__RightPointer = rp
+
+def SetData(self, d):
+self.-_Data = d
+
+# treeclass definition
+Eclass TreeClass():
+def -_init__(self):
+self.__Tree = [Node(-1) for - in range (20)]
+self.-FirstNode = -1
+self.-_NumberNodes = 0
+# something
+def
+InsertNode(self, NewNode) :
+if
+self.__NumberNodes = 0:
+self.-_Tree[self.-_NumberNodes] = NewNode
+self.-_NumberNodes += 1
+self.-_FirstNode = 0
+else:
+self.-_Tree[self. __NumberNodes] = NewNode
+current = self.-_FirstNode
+
+while current != -1:
+previous = current
+
+if NewNode._Data > self.-_Tree[current] .GetData():
+current = self.__Tree[current].Getright)
+
+direction = "R"
+if NewNode.-_Data < self.-_Tree [current] GetData():
+current = self.__Tree[current] .GetLeft
